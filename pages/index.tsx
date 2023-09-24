5d3c03fb94df10c56647c1e881e2a1dc5e85a154
Lem Uhuru <lem@esy.com>
2023-09-23 22:11:59 -0400

    update header

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,9 @@ import cloudinary from '../utils/cloudinary'
 import getBase64ImageUrl from '../utils/generateBlurPlaceholder'
 import type { ImageProps } from '../utils/types'
 import { useLastViewedPhoto } from '../utils/useLastViewedPhoto'
+import Navbar from '../components/navbar'
+
+
 
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   const router = useRouter()
@@ -46,12 +49,51 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         <div className=" w-full ">
           <div className="header-container">
 
-            <header className="flex justify-start h-[60px] px-50 bg-white">
+            {/* <header className="flex justify-start h-[60px] px-50 bg-white">
               <div className="logo-container max-w-[100px]">
                 <img src="https://assets.codepen.io/9394943/color-logo-no-bg.svg" alt="Logo" className="w-full h-full" />
               </div>
-            </header>
-
+            </header> */}
+            
+            <header className="bg-white">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="https://assets.codepen.io/9394943/color-logo-no-bg.svg" alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <span className="sr-only">Open main menu</span>
+            {/* SVG for the menu icon */}
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <div className="relative">
+            <button type="button" className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
+              Product
+              {/* SVG for the dropdown arrow */}
+            </button>
+            <div className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+              {/* ... rest of the dropdown content */}
+            </div>
+          </div>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Features</a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Company</a>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </nav>
+      <div className="lg:hidden" role="dialog" aria-modal="true">
+        <div className="fixed inset-0 z-10"></div>
+        <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          {/* ... rest of the mobile menu content */}
+        </div>
+      </div>
+    </header>
             <div className="hero-container flex flex-col justify-center h-full w-full max-h-[500px] bg-black border-b border-gray-900 bg-no-repeat bg-center bg-cover min-h-[600px]" style={{ backgroundImage: "url('https://assets.codepen.io/9394943/laughing-santa-2.png')" }}>
               <div className="hero-content ml-[50px]">
               {/* backdrop-blur-[1px] */}
