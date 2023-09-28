a3c1e42c0dd0d0a33d3f8530c265d45d583f1141
Lem Uhuru <lem@esy.com>
2023-09-28 04:26:01 -0400

    update styles

--- src/components/Page/Blog/blogLayout.tsx
+++ src/components/Page/Blog/blogLayout.tsx
@@ -35,7 +35,7 @@ const articles = [
 const BlogLayout = () => {
     return (
 <div className="max-w-screen-xl mx-auto">
-            <div className="dark:bg-gray-900 flex-1 p-4 grid grid-cols-1 md:grid-cols-2 gap-5 xl:pt-[100px]">
+            <div className="dark:bg-gray-900 flex-1 p-4 grid grid-cols-1 md:grid-cols-2 gap-5 ">
                 {articles.map((article, index) => (
                     <article key={index} className="h-full rounded-xl dark:border-gray-600 flex flex-col items-center sm:items-start">
                         <Link href={article.url} className="h-full mb-2 xl:mb-0">
