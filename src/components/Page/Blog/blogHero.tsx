46a0f60ca864e248f48c463f225f6f8b78026039
Lem Uhuru <lem@esy.com>
2023-09-28 01:11:34 -0400

    update blog hero markup

--- src/components/Page/Blog/blogHero.tsx
+++ src/components/Page/Blog/blogHero.tsx
@@ -6,8 +6,8 @@ const BlogHero = () => {
         <section className="bg-white dark:bg-gray-900">
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-12 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7 xl:col-span-8">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Building digital products & brands.</h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Here at flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Learn</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Stay ahead of the curve with super simple tutorials on how to generate and customize your own Ai art each week.</p>
             <form action="#" className="">
                 <div className="flex items-center mb-3">
                     <div className="relative w-auto mr-3">
@@ -18,14 +18,14 @@ const BlogHero = () => {
                         <input className="block md:w-96 w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" name="member[email]" id="member_email" required={true} />
                     </div>
                     <div>
-                        <input type="submit" value="Try htmlFor free" className="px-5 py-3 text-sm font-medium text-center text-white rounded-lg cursor-pointer bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" name="member_submit" id="member_submit" />
+                        <input type="submit" value="Get Newsletter" className="px-5 py-3 text-sm font-medium text-center text-white rounded-lg cursor-pointer bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" name="member_submit" id="member_submit" />
                     </div>
                 </div>
-                <div className="text-sm text-left text-gray-500 dark:text-gray-300">Instant signup. No credit card required. <a href="#" className="text-primary-600 hover:underline dark:text-primary-500">Terms of Service</a> and <a className="text-primary-600 hover:underline dark:text-primary-500" href="#">Privacy Policy</a>.</div>
+                {/* <div className="text-sm text-left text-gray-500 dark:text-gray-300">Instant signup. No credit card required. <a href="#" className="text-primary-600 hover:underline dark:text-primary-500">Terms of Service</a> and <a className="text-primary-600 hover:underline dark:text-primary-500" href="#">Privacy Policy</a>.</div> */}
             </form>
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 xl:col-span-4 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mobile-app.svg" alt="phone illustration" />
+            <img src="https://assets.codepen.io/9394943/learn-creative-clip-art.png" alt="phone illustration" />
         </div>                
     </div>
 </section>)
--- src/components/Page/Blog/blogLayout.tsx
+++ src/components/Page/Blog/blogLayout.tsx
@@ -1,19 +1,52 @@
 import React from "react";
 
 
+const articles = [
+    {
+        title: "Our first office",
+        image: "https://assets.codepen.io/9394943/santa-smiles-icons-white-bg.png",
+        description: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+        readTime: "2 minutes"
+    },
+    {
+        title: "Enterprise design tips",
+        image: "https://assets.codepen.io/9394943/christmas-tree-cookie-wbg.png",
+        description: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+        readTime: "12 minutes"
+    },
+    {
+        title: "We partnered up with Google",
+        image: "https://assets.codepen.io/9394943/christmas-tree-cookie-wbg.png",
+        description: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+        readTime: "8 minutes"
+    },
+    {
+        title: "Our first project with React",
+        image: "https://assets.codepen.io/9394943/santa-smiles-icons-white-bg.png",
+        description: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+        readTime: "12 minutes"
+    }
+];
+
 const BlogLayout = () => {
     return (
-            <div className= "dark:bg-gray-900 flex-1 p-4 grid grid-cols-1 md:grid-cols-2 gap-5 pt-[100px]">
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
-                <div className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96"></div>
+        <div className="dark:bg-gray-900 flex-1 p-4 grid grid-cols-1 md:grid-cols-2 gap-5 pt-[100px]">
+            {articles.map((article, index) => (
+                <article key={index} className="border-2 border-dashed border-gray-300 rounded-xl dark:border-gray-600 h-96 flex flex-col items-center sm:items-start xl:flex-row">
+                    <a href="#" className="mb-2 xl:mb-0">
+                        <img src={article.image} className="mr-5 max-w-sm" alt={`Image ${index + 1}`} />
+                    </a>
+                    <div className="flex flex-col justify-center items-center sm:items-start">
+                        <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">{article.title}</a>
+                        </h2>
+                        <p className="mb-4 text-gray-500 dark:text-gray-400 max-w-sm">{article.description}</p>
+                        <a href="#" className="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Read in {article.readTime}
+                        </a>
+                    </div>
+                </article>
+            ))}
         </div>
     )
 }
