f1f7a5e786dee3cc556a1ea5227fb1d9877b5960
Lem Uhuru <lem@esy.com>
2023-10-03 00:04:25 -0400

    update image gallery grid col

--- src/components/imageGallery.tsx
+++ src/components/imageGallery.tsx
@@ -30,7 +30,7 @@ const ImageGallery: React.FC<ImageGalleryProps> = ({
         <h2 id={`${categoryTitle.toLowerCase()}-clipart`} className="category-title w-full text-white flex justify-center text-2xl font-black p-[50px]">{`${categoryTitle} Clip Art`}</h2>
         
         <div className="flex flex-grow items-center justify-center "> {/* Added this container */}
-            <div className="grid grid-cols-3 xl:grid-cols-3 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-3 gap-4">
                 {imagesToDisplay.map((image, index) => (
                     <div key={index} className="rounded-lg mb-4 max-w-[516px] p-1 bg-gray-100 ">
                         <Image
