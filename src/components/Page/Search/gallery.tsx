9d8efcda413029ad7dcc646486ceb0dbaf3d03e4
Lem Uhuru <lem@esy.com>
2023-10-06 14:14:48 -0400

    update gallery styles

--- src/components/Page/Search/gallery.tsx
+++ src/components/Page/Search/gallery.tsx
@@ -19,14 +19,14 @@ const Gallery: React.FC<GalleryProps> = ({ searchResults }) => {
     }
 
     return (
-        <main className="bg-gray-50 dark:bg-gray-900 p-4 lg:mr-16 min-h-full pt-20">
-            <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-4 mb-4">
+        <main className="bg-gray-50 dark:bg-gray-900 p-4 lg:ml-16 lg:mr-16 min-h-full pt-20">
+            <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-6 mb-4">
                 {imagesToDisplay?.map((item, index) => {
                     // If the item has a document, use that. If not, just use the item.
                     const image = item.document ? item.document : item;
 
                     return (
-                        <div key={image.id || index} className="p-2 bg-gray-100 rounded-lg">
+                        <div key={image.id || index} className="p-1 bg-gray-100 rounded-lg">
                             <Image
                                 src={image.image_url || ''}
                                 alt={image.title || 'Image'}
